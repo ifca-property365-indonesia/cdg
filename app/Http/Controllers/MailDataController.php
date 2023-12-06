4915305651cb1fdc2843a03cd9070948f27baa1e
fandynasution <ahmad.ariffandy@ifca.co.id>
2023-12-06 11:12:41 +0700

    06122023 1107

--- app/Http/Controllers/MailDataController.php
+++ app/Http/Controllers/MailDataController.php
@@ -9,7 +9,7 @@ class MailDataController extends Controller
     public function receive(Request $request)
     {
         if ($request->type == "Q" && $request->module == "PO") {
-            return "PO REQUEST";
+            return redirect()->action([SecondController::class, 'methodName']);
         } else if ($request->type == "A" && $request->module == "PO"){
             return "PO ORDER";
         } else {
---
+++ app/Http/Controllers/PoRequestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PoRequestController extends Controller
+{
+    //
+}
